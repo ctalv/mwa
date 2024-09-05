c2dc041b3c3a11497231702e9e9b6344e35c0835
Claire Alverson <clairetalverson@gmail.com>
2024-09-04 22:02:05 -0500

    Added base about page

--- mwa/src/app/projects/page.tsx
+++ mwa/src/app/projects/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function Page() {
-    return <p>Projects</p>;
+    return (
+      <div className={styles.projects}>
+        <h1>
+          Projects
+        </h1>
+      </div>
+    );
   }
