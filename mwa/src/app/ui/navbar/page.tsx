1898607282ce0b08ca9b2fb1955ff225e0bfdb6e
Claire Alverson <clairetalverson@gmail.com>
2024-09-10 20:33:46 -0500

    navbar improved

--- mwa/src/app/ui/navbar/page.tsx
+++ mwa/src/app/ui/navbar/page.tsx
@@ -4,35 +4,37 @@ import styles from "./page.module.css";
 
 export default function Page() {
     return (
-    <nav className={styles.navcontainer}>
-        <div className={styles.navbar}>
-            <div >
+        <div className={styles.navcontainer}>
             <Image
-          className={styles.navlogo}
-          src="/MWA-logomark-saddle.png"
-          alt="Splash image placeholder"
-          width={1000}
-          height={1000}
-          priority
-        />
-            </div>
-            <div className={styles.navmenu}>
-                <ul className={styles.navlist}>
-                    <li className={styles.navitem}>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li className={styles.navitem}>
-                        <Link href="/projects">Projects</Link>
-                    </li>
-                    <li className={styles.navitem}>
-                        <Link href="/about">About</Link>
-                    </li>
-                    <li className={styles.navitem}>
-                        <Link href="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </div>
+                className={styles.navlogo}
+                src="/MWA-logomark-saddle.png"
+                alt="Splash image placeholder"
+                width={1000}
+                height={1000}
+                priority
+            />
+
+                <label className={styles.hamburger}>
+                    <input type='checkbox' />
+                </label>
+
+                <menu className={styles.navlist}>
+                    <nav>
+                        <li className={styles.navitem}>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className={styles.navitem}>
+                            <Link href="/projects">Projects</Link>
+                        </li>
+                        <li className={styles.navitem}>
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li className={styles.navitem}>
+                            <Link href="/contact">Contact</Link>
+                        </li>
+                    </nav>
+                </menu>
+
         </div>
-    </nav>
     );
-  }
+}
