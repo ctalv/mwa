59bb4e5cd9a274c56f851a3885054e91fed2d884
Claire Alverson <clairetalverson@gmail.com>
2025-01-13 11:29:59 -0600

    uploaded all of Rice Blvd

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,12 +1,25 @@
-import riceBlvd2 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-2.jpg"
-import riceBlvd22 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-22.jpg"
-import riceBlvd33 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-33.jpg"
-import riceBlvd35 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-35.jpg"
+import riceBlvd1 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas.jpg";
+import riceBlvd4 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-4.jpg";
+import riceBlvd5 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-5.jpg";
+import riceBlvd7 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-7.jpg";
+import riceBlvd11 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-11.jpg";
+import riceBlvd12 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-12.jpg";
+import riceBlvd15 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-15.jpg";
+import riceBlvd21 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-21.jpg";
+import riceBlvd23 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-23.jpg";
+import riceBlvd24 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-24.jpg";
+import riceBlvd26 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-26.jpg";
+import riceBlvd28 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-28.jpg";
+import riceBlvd29 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-29.jpg";
+import riceBlvd31 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-31.jpg";
+import riceBlvd33 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-33.jpg";
+import riceBlvd36 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-36.jpg";
+import riceBlvd39 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-39.jpg";
 
 const projectList = [
     {
         project: "Rice Village",
-        image: riceBlvd2,
+        image: riceBlvd1,
         alt: "Rice Boulevard main image.",
         description: "This is where you will do a description of your project. Rice Blvd"
     }
@@ -15,23 +28,31 @@ const projectList = [
 const tagList = [
     {
         id: 0,
-        tag: "Kitchen"
+        tag: "Detail",
     },
     {
         id: 1,
-        tag: "Bathroom"
+        tag: "Kitchen"
     },
     {
         id: 2,
-        tag: "Living"
+        tag: "Bathroom"
     },
     {
         id: 3,
-        tag: "Dining"
+        tag: "Living"
     },
     {
         id: 4,
-        tag: "Details"
+        tag: "Dining"
+    },
+    {
+        id: 5,
+        tag: "Entryway"
+    },
+    {
+        id: 6,
+        tag: "Office"
     }
 ]
 
@@ -39,26 +60,105 @@ const imageList = [
     {
         project: "Rice Village",
         alt: "Stylish kitchen island",
-        tag: 0,
-        image: riceBlvd2
+        tag: 1,
+        image: riceBlvd1
     },
     {
         project: "Rice Village",
         alt: "Lamp on a wall",
-        tag: 4,
-        image: riceBlvd22
+        tag: 1,
+        image: riceBlvd4
     },
     {
         project: "Rice Village",
         alt: "Chair with fireplace",
-        tag: 2,
-        image: riceBlvd33
+        tag: 1,
+        image: riceBlvd5
     },
     {
         project: "Rice Village",
         alt: "Wicker chair dining area",
+        tag: 1,
+        image: riceBlvd7
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 1,
+        image: riceBlvd11
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 1,
+        image: riceBlvd12
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 0,
+        image: riceBlvd15
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 0,
+        image: riceBlvd21
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 0,
+        image: riceBlvd23
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 5,
+        image: riceBlvd24
+    },
+
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 5,
+        image: riceBlvd26
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 4,
+        image: riceBlvd28
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 4,
+        image: riceBlvd29
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 6,
+        image: riceBlvd31
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
         tag: 3,
-        image: riceBlvd35
+        image: riceBlvd33
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 4,
+        image: riceBlvd36
+    },
+    {
+        project: "Rice Village",
+        alt: "Stylish kitchen island",
+        tag: 2,
+        image: riceBlvd39
     }
 ]
 
--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,28 +1,28 @@
-import importAll from "../scripts/utils.js";
-import riceBlvdMain from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-2.jpg"
-import riceBlvd1 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-22.jpg"
-import riceBlvd2 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-33.jpg"
-import riceBlvd3 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-35.jpg"
+// import importAll from "../scripts/utils.js";
+// import riceBlvdMain from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-2.jpg"
+// import riceBlvd1 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-22.jpg"
+// import riceBlvd2 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-33.jpg"
+// import riceBlvd3 from "../images/projects/Rice_Blvd/RachelAlysePhotographyTheOwenGroupChristmas-35.jpg"
 
-// const NAMEImages = importAll(require.context('../images/projects/DIRECTORY', false, '/\.jpg/'));
-// const riceBlvdImages = importAll(require.context('../images/projects/Rice_Blvd', false, '/\.jpg$/'));
+// // const NAMEImages = importAll(require.context('../images/projects/DIRECTORY', false, '/\.jpg/'));
+// // const riceBlvdImages = importAll(require.context('../images/projects/Rice_Blvd', false, '/\.jpg$/'));
 
 
-const projectList = [
-    {
-        title: "Rice Blvd",
-        mainImage: riceBlvdMain,
-        mainAlt: "Rice Boulevard main image.",
-        galleryImages: [riceBlvd1, riceBlvd2, riceBlvd3],
-        description : "This is where you will do a description of your project. Rice Blvd"
-    },
-    {
-        title: "Rice Blvd 2",
-        mainImage: riceBlvdMain,
-        mainAlt: "Rice Boulevard main image.",
-        galleryImages: [riceBlvd1, riceBlvd2, riceBlvd3],
-        description : "This is where you will do a description of your project. Rice Blvd"
-    },
-]
+// const projectList = [
+//     {
+//         title: "Rice Blvd",
+//         mainImage: riceBlvdMain,
+//         mainAlt: "Rice Boulevard main image.",
+//         galleryImages: [riceBlvd1, riceBlvd2, riceBlvd3],
+//         description : "This is where you will do a description of your project. Rice Blvd"
+//     },
+//     {
+//         title: "Rice Blvd 2",
+//         mainImage: riceBlvdMain,
+//         mainAlt: "Rice Boulevard main image.",
+//         galleryImages: [riceBlvd1, riceBlvd2, riceBlvd3],
+//         description : "This is where you will do a description of your project. Rice Blvd"
+//     },
+// ]
 
-export default projectList
+// export default projectList
