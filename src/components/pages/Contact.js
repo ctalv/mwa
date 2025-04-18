681434df44aba95800125c7df722b0264b0de6e5
ctalv <clairetalverson@gmail.com>
2025-04-18 10:25:37 -0500

    Refactored contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,34 +1,46 @@
 import React from "react";
 import "../../assets/styles/contact.css";
-
+import riceBlvd from "../../assets/images/Warren.jpg"
+import contactImage from "../../assets/images/projects/RSF/IMG_6924.jpeg"
 
 function Contact() {
 
-    
+
 
     return (
         <main>
-        <div className="contact-us container">
-            <h2>CONTACT US</h2>
-            <p>
-            If you would like to contact us about a project feel free to fill out the below.
-            </p>
-            <form id="contact-form" action="send_email.php"  method="post">
-                <label for="firstname" className="form-label">First Name</label>
-                <input type="text" id="firstname" name="firstname" className="form-control" required/>
-                <label for="lastname">Last Name</label>
-                <input type="text" id="lastname" name="lastname"className="form-control" required/>
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" className="form-control" required/>
-                <label for="message">Phone</label>
-                <input type="phone" id="phone" name="phone" className="form-control" required/>
-                <label for="message">Message</label>
-                <textarea id="message" name="message" placeholder="What would you like us to know?" className="form-control" required></textarea>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+            <div className="contact-us-page container ">
+                <div className="row">
+                    <div className="col-4">
+                        <img className="contact-us-img" src={contactImage}
+                        />
+                    </div>
+
+                    <div className="contact-us col-8">
+                        <h2>CONTACT US</h2>
+                        <p>
+                            If you would like to contact us about a project feel free to fill out the below.
+                        </p>
+                        <form id="contact-form" action="send_email.php" method="post">
+                            <label for="firstname" className="form-label">First Name</label>
+                            <input type="text" id="firstname" name="firstname" className="form-control" required />
+                            <label for="lastname">Last Name</label>
+                            <input type="text" id="lastname" name="lastname" className="form-control" required />
+                            <label for="email">Email</label>
+                            <input type="email" id="email" name="email" className="form-control" required />
+                            <label for="message">Phone</label>
+                            <input type="phone" id="phone" name="phone" className="form-control" required />
+                            <label for="message">Message</label>
+                            <textarea id="message" name="message" placeholder="What would you like us to know?" className="form-control" required></textarea>
+                            <button className="myButton" type="submit">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </main>
     );
 }
+
+// Side by side image and contact us
 
 export default Contact;
