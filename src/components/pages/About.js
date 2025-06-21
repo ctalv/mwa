00e4150d05d0d16224bd72e3d926c6e630067be0
ctalv <clairetalverson@gmail.com>
2025-06-20 19:56:33 -0500

    Aligned about page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,6 +10,41 @@ import itemLists from "../../assets/data";
 
 
 function About() {
+
+
+    function is_leader(person) {
+        let name = person.name
+        if (name == "Matthew Mitchell" || name == "Austin Wilson") {
+            return (
+
+                <div className="leader col-6 profile">
+                            <img className="headshot" src={person.headshot} alt="headshot" />
+                            <div className="titles bg-color-about color-about">
+                                <h3 className="person-name">{person.name}</h3>
+                                <h3 className="person-title">{person.title}</h3>
+                            </div>
+                            <div className="description bg-color-about">{person.description}</div>
+                        </div>
+
+                
+            )
+        }else{
+            return (
+                <div className="associate col-3 profile">
+                            <img className="headshot h-50" src={person.headshot} alt="headshot" />
+                            <div className="titles bg-color-about color-about">
+                                <h3 className="person-name">{person.name}</h3>
+                                <h3 className="person-title">{person.title}</h3>
+                            </div>
+                            <div className="description bg-color-about">{person.description}</div>
+                        </div>
+
+                
+            )
+        }
+    }
+
+
     const people = itemLists.about
     return (
         <main>
@@ -25,17 +60,8 @@ function About() {
                         cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                 </div>
-                <div className="people">
-                    {people.map(person => (
-                        <div className="profile " >
-                            <img className="headshot " src={person.headshot} alt="headshot" />
-                            <div className="titles bg-color-about color-about">
-                                <h3 className="person-name">{person.name}</h3>
-                                <h3 className="person-title">{person.title}</h3>
-                            </div>
-                            <div className="description bg-color-about">{person.description}</div>
-                        </div>
-                    ))}
+                <div className="people row">
+                    {people.map(person => (is_leader(person)))}
                             
                 </div>
             </div>
