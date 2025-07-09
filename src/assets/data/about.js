a5a0d005c5a986d7e00b33dc9958fbbcd8765763
ctalv <clairetalverson@gmail.com>
2025-07-08 22:24:58 -0500

    Added new people

--- src/assets/data/about.js
+++ src/assets/data/about.js
@@ -13,33 +13,51 @@ const aboutList = [
     },
     {
         name: "Warren Alverson",
-        title: "Associate",
+        title: "Project Manager",
         headshot: require("../images/Warren.jpg"),
         description: "Warren Alverson graduated with a Professional Bachelor’s degree in Architecture from theUniversity of Houston, and produced an undergraduate thesis centered around design inHoustonand Coastal Texas. Warren has experience in commercial and residential architecture inHoustonand Galveston. In his free time, Warren designs and builds furniture for himself and hisfamily.He also enjoys vacationing at National Parks and abroad with his wife, kayak fishing in theTexas bays, and spending time with his family and friends."
     },
     {
-        name: "Kate Wierzbiki",
-        title: "Associate",
-        headshot: require("../images/Matthew.jpg"),
-        description: "Boss Ass Bitch"
-    },
-    {
-        name: "Elvia Melendez",
-        title: "Associate",
-        headshot: require("../images/Elvia.jpg"),
-        description: "Elvia Melendez joined Matthew Mitchell Architecture in 2021 as an Architectural Associate.Sheis a Houston native with roots to Mexico who graduated from the University of Houston in2020with a Bachelors in Architecture. Her interest in architecture design springs from hereagernessto learn, she is particularly fond of art and history. With these as her foundation, she hasbecome an avid traveler, using her new surroundings as an opportunity to learn andincorporatethe new found knowledge into her own work. In her spare time, when she's not planning hernextadventure, she enjoys walking her dogs, reading books, going to concerts, catching up withfriends, and being in the company of her family."
+        name: "Emilia Cavallaro",
+        title: "Project Manager",
+        headshot: require("../images/Emilia.jpg"),
+        description: "Emilia Cavallaro earned her Bachelor of Architecture degree at Rice University in Houston,Texas, and has experience working in residential and commercial design in Houston and NewYork.Emilia has a particular interest in residential and interior design, as well as painting andartcuration. She and her wife enjoy traveling, cooking different cuisines, and spending timewith their two cats."
     },
     {
         name: "Meagan Dinh",
-        title: "Associate",
+        title: "Project Manager",
         headshot: require("../images/Meagan.jpg"),
         description: "Meagan Dinh joined Matthew Mitchell Architecture as an intern in 2021. She received herMaster’sin Land and Property Development at Texas A&M University in 2022, where she gained knowledgeinthe real estate industry, community design, and construction development. Her study abroadexperience in 2020 in Italy fostered her interest in residential design and its resultingeffectin community development. While born and raised in Texas, she enjoys traveling the world,spending time with her family and baking for her community."
     },
     {
-        name: "Emilia Cavallaro",
+        name: "Elvia Melendez",
+        title: "Project Manager",
+        headshot: require("../images/Elvia.jpg"),
+        description: "Elvia Melendez joined Matthew Mitchell Architecture in 2021 as an Architectural Associate.Sheis a Houston native with roots to Mexico who graduated from the University of Houston in2020with a Bachelors in Architecture. Her interest in architecture design springs from hereagernessto learn, she is particularly fond of art and history. With these as her foundation, she hasbecome an avid traveler, using her new surroundings as an opportunity to learn andincorporatethe new found knowledge into her own work. In her spare time, when she's not planning hernextadventure, she enjoys walking her dogs, reading books, going to concerts, catching up withfriends, and being in the company of her family."
+    },
+    {
+        name: "Kate Wierzbiki",
+        title: "Project Manager",
+        headshot: require("../images/Matthew.jpg"),
+        description: "Boss Ass Bitch"
+    },
+    {
+        name: "Derrick Banks",
         title: "Associate",
-        headshot: require("../images/Emilia.jpg"),
-        description: "Emilia Cavallaro earned her Bachelor of Architecture degree at Rice University in Houston,Texas, and has experience working in residential and commercial design in Houston and NewYork.Emilia has a particular interest in residential and interior design, as well as painting andartcuration. She and her wife enjoy traveling, cooking different cuisines, and spending timewith their two cats."
+        headshot: require("../images/Matthew.jpg"),
+        description: ""
+    },
+    {
+        name: "Rebecca Ramos",
+        title: "Associate",
+        headshot: require("../images/Matthew.jpg"),
+        description: ""
+    },
+    {
+        name: "Savon Reed",
+        title: "Associate",
+        headshot: require("../images/Matthew.jpg"),
+        description: ""
     },
     // {
     //     name: "Matthew Mitchell",
