0075db3bfb7c8353e668fb8b057c50146e2e70b4
ctalv <clairetalverson@gmail.com>
2025-07-08 22:17:37 -0500

    Description properly displaying for about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -23,7 +23,7 @@ function About() {
                                 <h3 className="person-name">{person.name}</h3>
                                 <h3 className="person-title">{person.title}</h3>
                             </div>
-                            {/* <div className="description bg-color-about">{person.description}</div> */}
+                            <div className="description bg-color-about">{person.description}</div>
                         </div>
 
                 
@@ -36,7 +36,7 @@ function About() {
                         <h3 className="person-name">{person.name}</h3>
                         <h3 className="person-title">{person.title}</h3>
                     </div>
-                    {/* <div className="description bg-color-about">{person.description}</div> */}
+                    <div className="description bg-color-about">{person.description}</div>
                 </div>
 
                 
